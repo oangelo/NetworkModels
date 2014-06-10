be317b4dc93215eb2ef141984925908b127169cb
oangelo <oangelo@gmail.com>
2014-06-10 11:37:41 -0300

    Adding square lattice network

---
+++ src/square.cpp
@@ -0,0 +1,18 @@
+#include "square.h"
+
+Square::Square(unsigned nodes)
+:Network(nodes)
+{
+    unsigned m(sqrt(nodes));
+    for (size_t i = 0; i < nodes; ++i)
+    {
+        if(i > 0 and i % m != 0)
+            CreateEdge(&((*this)[i]), &((*this)[i - 1]));
+        if((i + 1) % m != 0)
+            CreateEdge(&((*this)[i]), &((*this)[i + 1]));
+        if(i >= m)
+            CreateEdge(&((*this)[i]), &((*this)[i - m]));
+        if(i < nodes - m - 1)
+            CreateEdge(&((*this)[i]), &((*this)[i + m]));
+    }
+}
---
+++ src/square.h
@@ -0,0 +1,17 @@
+#ifndef SQUARE_H
+#define SQUARE_H 
+
+#include <random>
+#include <iostream>
+
+#include "network.h"
+
+
+class Square: public Network
+{
+    public:
+        Square(unsigned nodes);
+    private:
+};
+
+#endif /* SQUARE_H */
