8917fdbf12d12308b6796038f5c116be16ccfa27
oangelo <oangelo@gmail.com>
2014-12-17 14:00:57 -0200

    Correcting the cluster function

--- src/measures/clusters.h
+++ src/measures/clusters.h
@@ -10,6 +10,17 @@ Burn(network_models::Vertex& target, bool(*comparison)(network_models::Vertex& a
 
 template <typename vertex_type>
 std::vector<std::unordered_set<network_models::Vertex*>> 
-Clusters(network_models::Network<vertex_type>& network, bool(*comparison)(network_models::Vertex& a, network_models::Vertex& b));
+Clusters(network_models::Network<vertex_type>& network, bool(*comparison)(network_models::Vertex& a, network_models::Vertex& b)){
+  std::unordered_set<network_models::Vertex*> burned;
+  std::vector<std::unordered_set<network_models::Vertex*>> clusters;
+  for(auto& i: network){
+    if(burned.find(&i) == burned.end()){
+      auto aux(Burn(i, comparison));
+      clusters.push_back(aux);
+      burned.insert(aux.begin(), aux.end());
+    }
+  }
+  return clusters;
+}
 
 #endif /* CLUSTERS_H */
