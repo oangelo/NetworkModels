bdcd2f44899f324452ae4598f6ef13e938d8667a
oangelo <oangelo@gmail.com>
2015-11-24 17:54:04 -0200

    Adding a network model of a toroid

--- src/models/square.h
+++ src/models/square.h
@@ -31,6 +31,32 @@ namespace network_models{
     }
   }
 
+  template<typename Node>
+  class Toroid: public Network<Node>
+  {
+    public:
+      Toroid(unsigned nodes);
+    private:
+  };
+
+  template<typename Node>
+  Toroid<Node>::Toroid(unsigned nodes)
+    :Network<Node>(nodes, "square")
+  {
+    unsigned m(sqrt(nodes));
+    for (size_t i = 0; i < nodes; ++i)
+    {
+        if((i + 1) % m != 0)
+          this->CreateUndirectedEdge(&((*this)[i]), &((*this)[i + 1]));
+        else
+          this->CreateUndirectedEdge(&((*this)[i]), &((*this)[i - (static_cast<double>(i)/m)*m]));
+        if(i >= m)
+          this->CreateUndirectedEdge(&((*this)[i]), &((*this)[i - m]));
+        else
+          this->CreateUndirectedEdge(&((*this)[i]), &((*this)[nodes - (m - i)]));
+    }
+  }
+
 
 } //namespace end
 #endif /* SQUARE_H */
